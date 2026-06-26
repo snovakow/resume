db6334d385452b566a8d3c110358a872ae79ad64
snovakow <snovakow@gmail.com>
2026-06-26 14:31:27 -0400

    Update main.ts
    Doc formatting

--- src/main.ts
+++ src/main.ts
@@ -32,10 +32,10 @@ const contactLines = `+1 (647) 465-3759
 snovakow@gmail.com
 Toronto, ON, Canada`.split('\n');
 for (const line of contactLines) {
-  // let hidden = "";
-  // for (let i = 0; i < line.length; i++) hidden += "x";
-  contact.appendChild(document.createTextNode(line));
-  contact.appendChild(document.createElement('br'));
+	// let hidden = "";
+	// for (let i = 0; i < line.length; i++) hidden += "x";
+	contact.appendChild(document.createTextNode(line));
+	contact.appendChild(document.createElement('br'));
 }
 
 const content = document.createElement('div');
@@ -43,45 +43,45 @@ content.className = 'content';
 page.appendChild(content);
 
 const addHeader = (text: string) => {
-  const header = document.createElement('div');
-  header.className = 'header';
-  header.appendChild(document.createTextNode(text));
-  content.appendChild(header);
+	const header = document.createElement('div');
+	header.className = 'header';
+	header.appendChild(document.createTextNode(text));
+	content.appendChild(header);
 }
 
 const addParagraph = (text: string) => {
-  const paragraph = document.createElement('div');
-  paragraph.className = 'paragraph';
-  paragraph.appendChild(document.createTextNode(text));
-  content.appendChild(paragraph);
+	const paragraph = document.createElement('div');
+	paragraph.className = 'paragraph';
+	paragraph.appendChild(document.createTextNode(text));
+	content.appendChild(paragraph);
 }
 
 const link = (url: string, title?: string) => {
-  title ??= url;
-  const a = document.createElement('a');
-  a.href = url;
-  a.textContent = title;
-  a.target = "_blank";
-  return a;
+	title ??= url;
+	const a = document.createElement('a');
+	a.href = url;
+	a.textContent = title;
+	a.target = "_blank";
+	return a;
 }
 
 type RecursiveArray<T> = Array<T | RecursiveArray<T>>;
 type RecursiveArrayType = string | Node;
 const createItem = (item: RecursiveArray<RecursiveArrayType> | RecursiveArrayType) => {
-  if (Array.isArray(item)) {
-    const ul = document.createElement('ul');
-    ul.className = 'bulletMain';
-    for (const element of item) ul.appendChild(createItem(element));
-    return ul;
-  }
+	if (Array.isArray(item)) {
+		const ul = document.createElement('ul');
+		ul.className = 'bulletMain';
+		for (const element of item) ul.appendChild(createItem(element));
+		return ul;
+	}
 
-  const li = document.createElement('li');
-  li.className = 'bulletSub';
+	const li = document.createElement('li');
+	li.className = 'bulletSub';
 
-  if (typeof item === 'string') li.appendChild(document.createTextNode(item));
-  else li.appendChild(item);
+	if (typeof item === 'string') li.appendChild(document.createTextNode(item));
+	else li.appendChild(item);
 
-  return li;
+	return li;
 }
 
 addHeader('SUMMARY');
@@ -95,85 +95,85 @@ addParagraph('I have supervised interns and mentored summer students throughout 
 
 addHeader('EXPERIENCE');
 const boldSection = (text: string) => {
-  const bold = document.createElement('span');
-  bold.className = 'header';
-  bold.appendChild(document.createTextNode(text));
-  return bold;
+	const bold = document.createElement('span');
+	bold.className = 'header';
+	bold.appendChild(document.createTextNode(text));
+	return bold;
 }
 const experience = createItem([
-  boldSection('Senior Developer, Liquid Cinema — November 2015—Present'),
-  [
-    link('https://liquidcinemavr.com'),
-    'Built the frontend web component of the Liquid Cinema platform, a browser native interactive cinematic 360° video based WebGL player.',
-    'Heat Maps, a backend storage of user viewing directions, and frontend heat map styled visualization to represent concentrations of user viewing directions.',
-    'Responsible for client side Apple iOS and tvOS components of the Liquid Cinema platform.',
-    'Developed Virtual and Augmented Reality support for the web component of the Liquid Cinema platform using the WebXR standard.',
+	boldSection('Senior Developer, Liquid Cinema — November 2015—Present'),
+	[
+		link('https://liquidcinemavr.com'),
+		'Built the frontend web component of the Liquid Cinema platform, a browser native interactive cinematic 360° video based WebGL player.',
+		'Heat Maps, a backend storage of user viewing directions, and frontend heat map styled visualization to represent concentrations of user viewing directions.',
+		'Responsible for client side Apple iOS and tvOS components of the Liquid Cinema platform.',
+		'Developed Virtual and Augmented Reality support for the web component of the Liquid Cinema platform using the WebXR standard.',
 
-    'OMAF 360° VR Video Streaming',
-    [
-      'Fraunhofer HHI collaboration to integrate OMAF 360° VR video streaming into the Liquid Cinema web platform.',
-      link('https://www.hhi.fraunhofer.de/en/departments/vca/technologies-and-solutions/mpeg-omaf.html'),
-    ],
+		'OMAF 360° VR Video Streaming',
+		[
+			'Fraunhofer HHI collaboration to integrate OMAF 360° VR video streaming into the Liquid Cinema web platform.',
+			link('https://www.hhi.fraunhofer.de/en/departments/vca/technologies-and-solutions/mpeg-omaf.html'),
+		],
 
-    '"Magic of Flight" interactive VR educational experience, Lead WebXR Developer',
-    [
-      'Meta collaboration to create an interactive web experience for the Quest VR Headset.',
-      '2021 WebXR Poly Awards winner:',
-      [
-        'Education Experience of the Year',
-        'Video Experience of the Year',
-        'Experience of the Year',
-      ],
-      link('https://liquidcinemavr.com/fly/'),
-    ],
+		'"Magic of Flight" interactive VR educational experience, Lead WebXR Developer',
+		[
+			'Meta collaboration to create an interactive web experience for the Quest VR Headset.',
+			'2021 WebXR Poly Awards winner:',
+			[
+				'Education Experience of the Year',
+				'Video Experience of the Year',
+				'Experience of the Year',
+			],
+			link('https://liquidcinemavr.com/fly/'),
+		],
 
-    'Multi user WebRTC live web video chat integration using PeerJS, using the Colyseus and Feathers backend systems',
+		'Multi user WebRTC live web video chat integration using PeerJS, using the Colyseus and Feathers backend systems',
 
-    'Senior Metaverse collaborative environment frontend Lead',
-    [
-      'Conestoga College Collaboration.',
-      '3D Avatar conferencing solution.',
-      'Virtual Reality multi-user live interactive environment.',
-      'Messaging and live video and screen-share streaming.',
-    ],
-  ],
-  boldSection('University of Calgary; Calgary, AB — 2005—2015'),
-  [
-    'The Lindsay Project, Lead Programmer and Software Designer — 2009—2015',
-    [
-      'Funded by the department of Undergraduate Medical Education in the Cumming School of Medicine at the University of Calgary.',
-      'I worked with Zygote 3D human anatomy models, originally used in Google Body, now Zygote Body, as part of a full stack pipeline for multiple applications.',
-      'Developed “Zygote 3D Anatomy Atlas & Dissection Lab.”',
-      [
-        'iPhone application released commercially to the iTunes App Store.',
-        'Currently not available, but reference videos are available:',
-        link('https://www.youtube.com/@Zygote3D/videos'),
-      ],
-      'Developed "Atlas", a 3D Human anatomy education oriented web application.',
-      [
-        'CBC segment on The Lindsay Project, featuring the Atlas application:',
-        link('https://www.cbc.ca/news/canada/calgary/new-medical-tool-honours-u-of-c-student-s-memory-1.1240316'),
-      ],
-    ],
+		'Senior Metaverse collaborative environment frontend Lead',
+		[
+			'Conestoga College Collaboration.',
+			'3D Avatar conferencing solution.',
+			'Virtual Reality multi-user live interactive environment.',
+			'Messaging and live video and screen-share streaming.',
+		],
+	],
+	boldSection('University of Calgary; Calgary, AB — 2005—2015'),
+	[
+		'The Lindsay Project, Lead Programmer and Software Designer — 2009—2015',
+		[
+			'Funded by the department of Undergraduate Medical Education in the Cumming School of Medicine at the University of Calgary.',
+			'I worked with Zygote 3D human anatomy models, originally used in Google Body, now Zygote Body, as part of a full stack pipeline for multiple applications.',
+			'Developed “Zygote 3D Anatomy Atlas & Dissection Lab.”',
+			[
+				'iPhone application released commercially to the iTunes App Store.',
+				'Currently not available, but reference videos are available:',
+				link('https://www.youtube.com/@Zygote3D/videos'),
+			],
+			'Developed "Atlas", a 3D Human anatomy education oriented web application.',
+			[
+				'CBC segment on The Lindsay Project, featuring the Atlas application:',
+				link('https://www.cbc.ca/news/canada/calgary/new-medical-tool-honours-u-of-c-student-s-memory-1.1240316'),
+			],
+		],
 
-    'Vaccine Design and Implementation Project, University of Calgary; Calgary, AB — 2008',
-    [
-      'Developed interactive realtime 3D OpenGL visualizations to represent the modelling part of a vaccine design project funded through the AHFMR Interdisciplinary Team in Vaccine Design and Implementation program at the University of Calgary.',
-    ],
+		'Vaccine Design and Implementation Project, University of Calgary; Calgary, AB — 2008',
+		[
+			'Developed interactive realtime 3D OpenGL visualizations to represent the modelling part of a vaccine design project funded through the AHFMR Interdisciplinary Team in Vaccine Design and Implementation program at the University of Calgary.',
+		],
 
-    'Swarm Art Software Developer, University of Calgary; Calgary, AB — 2005—2007',
-    [
-      'Artistic based apps and tools that use agent based swarm systems to generate visuals and facilitate interactive displays.',
-      'Featured in:',
-      [
-        'Images published in the Leonardo Journal, Volume 40, issue 3, by MIT Press, 2007.',
-        'Digital’06: “Bio/Med SciART” submission accepted for public display in the New York Hall of Science, 2006.',
-        'Victoria Conference Center street front window interactive public display, Victoria BC, August to December 2006',
-        'Discovery Channel "A Daily Planet" segment on swarm intelligence, featuring the Swarm Art system, aired on October 23, 2006.',
-        'Nickle Galleries Museum interactive display, Calgary AB, 2005.',
-      ]
-    ],
-  ],
+		'Swarm Art Software Developer, University of Calgary; Calgary, AB — 2005—2007',
+		[
+			'Artistic based apps and tools that use agent based swarm systems to generate visuals and facilitate interactive displays.',
+			'Featured in:',
+			[
+				'Images published in the Leonardo Journal, Volume 40, issue 3, by MIT Press, 2007.',
+				'Digital’06: “Bio/Med SciART” submission accepted for public display in the New York Hall of Science, 2006.',
+				'Victoria Conference Center street front window interactive public display, Victoria BC, August to December 2006',
+				'Discovery Channel "A Daily Planet" segment on swarm intelligence, featuring the Swarm Art system, aired on October 23, 2006.',
+				'Nickle Galleries Museum interactive display, Calgary AB, 2005.',
+			]
+		],
+	],
 ]);
 content.appendChild(experience);
 
@@ -181,56 +181,56 @@ addHeader('EDUCATION');
 addParagraph('University of Calgary, Calgary, AB — Bachelor of Science in Computer Science, 2006.');
 
 const createPublication = (main: string, sub1a: string, subName: string, sub1b: string, sub2: string) => {
-  const mainElement = document.createElement('div');
-  mainElement.className = 'publicationMain';
-  mainElement.appendChild(document.createTextNode(main));
+	const mainElement = document.createElement('div');
+	mainElement.className = 'publicationMain';
+	mainElement.appendChild(document.createTextNode(main));
 
-  const subElement1 = document.createElement('div');
-  subElement1.className = 'publicationSub1';
+	const subElement1 = document.createElement('div');
+	subElement1.className = 'publicationSub1';
 
-  subElement1.appendChild(document.createTextNode(sub1a));
+	subElement1.appendChild(document.createTextNode(sub1a));
 
-  const subElementName = document.createElement('span');
-  subElementName.className = 'publicationName';
-  subElementName.appendChild(document.createTextNode(subName));
-  subElement1.appendChild(subElementName);
+	const subElementName = document.createElement('span');
+	subElementName.className = 'publicationName';
+	subElementName.appendChild(document.createTextNode(subName));
+	subElement1.appendChild(subElementName);
 
-  subElement1.appendChild(document.createTextNode(sub1b));
+	subElement1.appendChild(document.createTextNode(sub1b));
 
-  const subElement2 = document.createElement('div');
-  subElement2.className = 'publicationSub2';
-  subElement2.appendChild(document.createTextNode(sub2));
+	const subElement2 = document.createElement('div');
+	subElement2.className = 'publicationSub2';
+	subElement2.appendChild(document.createTextNode(sub2));
 
-  const element = document.createElement('div');
-  element.appendChild(mainElement);
-  element.appendChild(subElement1);
-  element.appendChild(subElement2);
-  return element;
+	const element = document.createElement('div');
+	element.appendChild(mainElement);
+	element.appendChild(subElement1);
+	element.appendChild(subElement2);
+	return element;
 }
 addHeader('PUBLICATIONS');
 const publications = createItem([
-  createPublication(
-    'LINDSAY Virtual Human: Multi-Scale, Agent-based, and Interactive',
-    'C. Jacob, S. von Mammen, T. Davison, A. Sarraf-Shirazi, V. Sarpe, A. Esmaeili, D. Phillips, I. Yazdanbod, ',
-    'S. Novakowski',
-    ', S. Steil, C. Gingras, H. Jamniczky, B. Hallgrimsson, and B. Wright.',
-    'Advances in Intelligent Modelling and Simulation, Studies in Computational Intelligence Volume 422, pp 327-349; Springer-Verlag Berlin Heidelberg, 2012',
-  ),
-  createPublication(
-    'Evolutionary Swarm Design: How Can Swarm-based Systems Help to Generate and Evaluate Designs?',
-    'Sebastian von Mammen, ', 'Scott Novakowski', ', Gerald Hushlak, and Christian Jacob.',
-    'Design Principles and Practices: An International Journal, Volume 3, Issue 3, pp. 371-386, 2009.',
-  ),
-  createPublication(
-    'Evolutionary design of dynamic SwarmScapes',
-    'Namrata Khemka, ', 'Scott Novakowski', ', Gerald Hushlak, and Christian Jacob.',
-    'Genetic and Evolutionary Computation Conference, GECCO 2008, Proceedings, Atlanta, GA, USA, July 12-16, 2008.',
-  ),
-  createPublication(
-    'Motion swarms: video interaction for art in complex environments',
-    'Quoc Nguyen, ', 'Scott Novakowski', ', Jeffrey E. Boyd, Christian Jacob, and Gerald Hushlak.',
-    'Proceedings of the 14th annual ACM international conference on Multimedia, Santa Barbara, CA, USA, October 23-27, 2006.',
-  ),
+	createPublication(
+		'LINDSAY Virtual Human: Multi-Scale, Agent-based, and Interactive',
+		'C. Jacob, S. von Mammen, T. Davison, A. Sarraf-Shirazi, V. Sarpe, A. Esmaeili, D. Phillips, I. Yazdanbod, ',
+		'S. Novakowski',
+		', S. Steil, C. Gingras, H. Jamniczky, B. Hallgrimsson, and B. Wright.',
+		'Advances in Intelligent Modelling and Simulation, Studies in Computational Intelligence Volume 422, pp 327-349; Springer-Verlag Berlin Heidelberg, 2012',
+	),
+	createPublication(
+		'Evolutionary Swarm Design: How Can Swarm-based Systems Help to Generate and Evaluate Designs?',
+		'Sebastian von Mammen, ', 'Scott Novakowski', ', Gerald Hushlak, and Christian Jacob.',
+		'Design Principles and Practices: An International Journal, Volume 3, Issue 3, pp. 371-386, 2009.',
+	),
+	createPublication(
+		'Evolutionary design of dynamic SwarmScapes',
+		'Namrata Khemka, ', 'Scott Novakowski', ', Gerald Hushlak, and Christian Jacob.',
+		'Genetic and Evolutionary Computation Conference, GECCO 2008, Proceedings, Atlanta, GA, USA, July 12-16, 2008.',
+	),
+	createPublication(
+		'Motion swarms: video interaction for art in complex environments',
+		'Quoc Nguyen, ', 'Scott Novakowski', ', Jeffrey E. Boyd, Christian Jacob, and Gerald Hushlak.',
+		'Proceedings of the 14th annual ACM international conference on Multimedia, Santa Barbara, CA, USA, October 23-27, 2006.',
+	),
 ]);
 content.appendChild(publications);
 
